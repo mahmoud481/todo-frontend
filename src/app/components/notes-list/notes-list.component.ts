1c045a14f3bd77874d284ac0267cc4a5e7acc371
mahmoud481 <mah.mohamed48@gmail.com>
2021-07-02 02:29:27 +0200

    retrieve all notes

--- src/app/components/notes-list/notes-list.component.ts
+++ src/app/components/notes-list/notes-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { NoteService } from './../../services/note.service';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _NoteService: NoteService,
+    private _NgbModal: NgbModal,
+    private _ToastrService: ToastrService
+  ) { }
 
+  allNotes: any;
   ngOnInit(): void {
+    this.getAll() // call one time in the component initialization
   }
+  // Get All Posts
+  getAll(): any {
+    this._NoteService.getAll().subscribe(res => {
+      this.allNotes = res;
+      this.allNotes = this.allNotes.data;
+    }, err => {
+      alert(err);
+    });
+  }
+
 
 }
